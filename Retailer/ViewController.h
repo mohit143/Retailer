f51789b245048f060848b5daab8f5fb85e3d48cd
Mohit Mathur <mathurmohit0@gmail.com>
2016-06-07 12:51:20 +0530

    Initial Commit

---
+++ Retailer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Retailer
+//
+//  Created by MOHIT MATHUR on 07/06/16.
+//  Copyright © 2016 mohit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Retailer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Retailer
+//
+//  Created by MOHIT MATHUR on 07/06/16.
+//  Copyright © 2016 mohit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
